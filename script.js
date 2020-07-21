347a76d7d8a64920c92a6efb73946c53d65b4e08
dnny60 <dnny60@gmail.com>
2020-07-21 20:34:10 +0800

    demo3

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ async function start() {  //function start()
 }
 
 function loadLabeledImages() {
-  const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark', 'DANNY']
+  const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark', 'DANNY', 'au', 'mo']
   return Promise.all(
     labels.map(async label => {
       const descriptions = []
